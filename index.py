723328e82417ed77818da763b915d2f506a3dbe3
taffy <taffy@zhihu.com>
2017-04-23 22:28:19 +0800

    add files

--- index.py
+++ index.py
@@ -1,3 +1,30 @@
-def application(environ, start_response):
-    start_response('200 OK', [('Content-Type', 'text/html')])
-    return [b'<h1>Hello, web!</h1>']
+# coding=UTF-8
+
+from flask import Flask
+from flask import request
+from flask import jsonify
+
+users = {}
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    return '<h1>Home</h1>'
+
+@app.route('/list', methods=['GET'])
+def list():
+    return jsonify(users)
+
+@app.route('/user', methods=['GET', 'POST'])
+def user():
+    u = (request.json)['user']
+    k = u['avatarUrl']
+    if k not in users:
+        users[k] = u
+        return 'success'
+    else:
+        return 'failure'
+
+if __name__ == '__main__':
+    app.run()
